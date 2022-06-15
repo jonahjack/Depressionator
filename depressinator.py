c910bec861ec454167fb481e331193e06c4e48be
jonahjack <youngthreatrapper@gmail.com>
2022-06-14 22:03:06 -0400

    Added detailed documentation

--- depressinator.py
+++ depressinator.py
@@ -34,143 +34,30 @@ triggerWord4 = ""
 isInsult = False
 
 
-def randomInsult():
-    
-    randomcol1 = random.choice(col1)
-    randomcol2 = random.choice(col2)
-    randomcol3 = random.choice(col3)
-    InsultFinal = "Thou " + randomcol1+ " " + randomcol2 + " " + randomcol3 + "!"
-    TTS(InsultFinal)
-
-def TTS(ins):
-    insulttts = gTTS(ins)
-    date_string = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-    filename = "voice"+date_string+".mp3"
-    insulttts.save(filename)
-    playsound.playsound(filename)
-    os.remove(filename)
-    get_Audio()
-
-
-
-def takeChannel():
-    webbrowser.open_new_tab('https://www.youtube.com/channel/UCEsSazCLxuFXYxZAx2BmOMQ')
-
-def takeTutorial():
-    webbrowser.open_new_tab('https://www.youtube.com/watch?v=zNFospmOFdg')
-
-def makeRegButton():
-    global regular_button
-    regular_button = tk.Button(root, text="Regular Mode", command= regularMode)
-    regular_button.grid(row=4, column=0,pady=3)
-
-def makeStreamButton():
-    global stream_button
-    stream_button = tk.Button(root, text="Streamer Mode(Coming Soon)", command= streamMode)
-    stream_button.grid(row=3,column=0,pady=3)
-
-def makeFrame():
-    global frame
-    frame = tk.Frame(root, width=600, height=445)
-    frame.grid(row=5,column=0, pady=15)
-
-
-def regularMode():
-    clearframe(clear=frame)
-    global regEntryText
-    regEntryText = tk.Label(frame, text="Trigger Word Count:")
-    regEntryText.pack()
-    global drop
-    drop = tk.OptionMenu( frame , clicked , *options )
-    drop.pack()
-    global regEntryButton
-    regEntryButton = tk.Button(frame, text="Enter Numbers", command=loadEntry)
-    regEntryButton.pack()
-    
-def loadEntry():
-    regEntryText.destroy()
-    drop.destroy()
-    regEntryButton.destroy()
-    global num
-    num = 0
-    global regEntryDic
-    regEntryDic = {}
-    global entered
-    entered = {}
-    for i in range(4):
-        num +=1
-        entered["Value%s" %num]= tk.StringVar()
-        entered["Value%s" %num].set("null")
-    num = 0
-    for i in range(clicked.get()):
-        num +=1
-        regEntryDic["regEntry%s" %num] = tk.Entry(frame)
-        regEntryDic["regEntry%s" %num].pack(pady=3)
-        regEntryDic["regEntry%s" %num].config(textvariable = entered["Value%s" %num])
-    global RegSubmit
-    RegSubmit = tk.Button(frame, text="Submit", command=regSubmitEntry)
-    RegSubmit.pack(pady=3)
-    
-def clearframe(clear):
-    for widgets in clear.winfo_children():
-      widgets.destroy()
-
-def regSubmitEntry():
-    clearframe(clear=frame)
-    num = 0
-    for i in range(clicked.get()):
-        num +=1
-        regEntryDic["regEntry%s" %num].destroy()
-        RegSubmit.destroy()
-    num = 0
-    for i in range(4):
-        num +=1
-    assignVariables()
-    closepage = tk.Label(frame, text="Now when you close this window it will listen for those words,\nif you want the program to stop close the terminal that pops up as well,\ndo not close the terminal until you want the program to stop.").pack()
-
-def streamMode():
-    clearframe(clear=frame)
-    comingsoon = tk.Label(frame, text="Coming Soon").pack()
-
-
-def speak(text):
-    ttsb = gTTS(text=text, lang="en")
-    filenameb = "voice.mp3"
-    ttsb.save(filenameb)
-    playsound.playsound(filenameb)
-
-def assignVariables():
-    num = 0
-    global values
-    values = {}
-    for i in range(4):
-        num+=1
-        values["val%s" %num] = entered["Value%s" %num].get()
-
-
-def get_Audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
+# speech_recognition functions
+def get_Audio(): # gets audio from user mic and parses it
+    r = sr.Recognizer() # used to get the audio library
+    with sr.Microphone() as source: # uses the sr.Microphone() function as source for refference
         
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-        said = ""
+        r.adjust_for_ambient_noise(source, duration=1) # adjusts for background noise
+        audio = r.listen(source) # makes a var audio that is using source as a listening device
+        said = "" # empty placeholder var
 
-        try:
-            said = r.recognize_google(audio)
-        except Exception as e:  
-            print("Exception" + str(e))
+        try: 
+            said = r.recognize_google(audio) # sets said to = tghe output of our audio passed throug a google api that tries to figure out what is being said
+        except Exception as e:
+            print("Exception" + str(e)) # if the api cant recognize the audio, or any other errors happen, it will print the error in the log instead of stopping the program
     
-    startVoice(audio=said)
+    startVoice(audio=said) # starts the filters for the trigegr words
 
-def startVoice(audio):
+def startVoice(audio): # filters the words in the sentence to see if they are any of the preset trigger words.
 
-    if triggerWord1 != "null":
-        if triggerWord1 in audio:
-            randomInsult()
-            return
+    if triggerWord1 != "null": # Checks to see if the word is set, the word will be "null" by default.
+        if triggerWord1 in audio: # Checks for the word in the sentence
+            randomInsult()  # if it finds the word
+            return # if it does it triggers the Insult generator and ends the cycle until insult is generated and spoken
         else:
-            pass
+            pass # if not conite to the next word and rinse and repeat for the rest
     if triggerWord2 != "null":
         if triggerWord2 in audio:
             randomInsult()
@@ -190,10 +77,144 @@ def startVoice(audio):
         else:
             pass
 
-    get_Audio()
+    get_Audio() # is it doest find a word it just restarts the whole loop
+
+# random insult genorator functions
+def randomInsult(): # Makes A Random Insult Using randon.choice and the columns of insults above
+    
+    randomcol1 = random.choice(col1) # chooses a random word from the columns above
+    randomcol2 = random.choice(col2) # chooses a random word from the columns above
+    randomcol3 = random.choice(col3) # chooses a random word from the columns above
+    InsultFinal = "Thou " + randomcol1+ " " + randomcol2 + " " + randomcol3 + "!" # puts the insult together
+    TTS(InsultFinal) # sends it to the TTS function
+
+# Google tts functions
+def TTS(ins): # Uses google TTS modual to turn our insult variable to audiable speech
+    insulttts = gTTS(ins) # initiates google tts
+    date_string = datetime.datetime.now().strftime("%d%m%Y%H%M%S") #creates a datestring for the file name to be unique
+    filename = "voice"+date_string+".mp3" # makes the name of the file for future use
+    insulttts.save(filename) # saves the file with the name made above
+    playsound.playsound(filename) # plays the file without need of groove music being opened
+    os.remove(filename) # deletes the file to keep folder size low
+    get_Audio() # starts the cycle all over again
+
+
+
+
+"""
+this is the explanation of everything in the tkinter section, as it would be painful to leave this as a string of hashtag im just using quotes to keep it uniform
+A quick overview of the libraries i used bellows' functions so you'll know what im doing bellow, i'll still leave comments in confusing areas tho
+tk = tkinter so anything starting with that is here
+.TK: Makes a window to host our widgets
+.Frame: Makes a frame similar to a TK, exept its soul purpose os to hold widgets but still be movable and seperate from the rest of the Window
+.Button: Makes a button
+.Label: Makes text
+.OptionMenu: Makes a Drop Down Menu
+.Entry: Makes a user input text field
+.StringVar: Makes a string variable that can be used by entry fields of any kind
+.IntVar: Same as .StringVar but for ints
+.grid: places the items on a predetermined grid instead of randomly, keeps things orginized
+.pack: places things in a top to bottom order in the middle on the x axis
+.destroy: destroys the widget
+.winfo_children: Gets a list of children in an object
+.geometry: sets size of a window
+.title: sets the name of a window
+.resizable: says if the window is resizable, and on what axis
+.mainloop: loops as long as tkinter window is open
+
+webbrowser: opens a tab on the browser for the link you set
+
+
+"""
+
+
+# webbrowser functions
+def takeChannel():
+    webbrowser.open_new_tab('https://www.youtube.com/channel/UCEsSazCLxuFXYxZAx2BmOMQ')
+
+def takeTutorial():
+    webbrowser.open_new_tab('https://www.youtube.com/watch?v=zNFospmOFdg')
+
+# Tkinter Functions
+def clearframe(clear): # clears the frame of widgets
+    for widgets in clear.winfo_children():
+      widgets.destroy()
+
+def makeRegButton(): # makes the Regular mode buttons
+    global regular_button
+    regular_button = tk.Button(root, text="Regular Mode", command= regularMode)
+    regular_button.grid(row=4, column=0,pady=3)
+
+def makeStreamButton(): # makes the Twitch mode buttons
+    global stream_button
+    stream_button = tk.Button(root, text="Twitch Mode(Coming Soon)", command= streamMode)
+    stream_button.grid(row=3,column=0,pady=3)
+
+def makeFrame(): # makes the frame used for either mode 
+    global frame
+    frame = tk.Frame(root, width=600, height=445)
+    frame.grid(row=5,column=0, pady=15)
+
+
+def regularMode(): # makes the menu and variables for the regular mode
+    clearframe(clear=frame) # clears the frame for the new menu to be created
+    global regEntryText
+    regEntryText = tk.Label(frame, text="Trigger Word Count:")
+    regEntryText.pack()
+    global drop
+    drop = tk.OptionMenu( frame , clicked , *options )
+    drop.pack()
+    global regEntryButton
+    regEntryButton = tk.Button(frame, text="Enter Numbers", command=loadEntry)
+    regEntryButton.pack()
+    
+def loadEntry():  # makes the entry fields and buttons needed to make the new menu using loops that are determined by the number you chose before
+    regEntryText.destroy()
+    drop.destroy()
+    regEntryButton.destroy()
+    global num
+    num = 0  #num is used allot in this script, it keeps indexes for loops so the dictionaries used can have incremental results, any time you see null = 0 its resetting the index
+    global regEntryDic
+    regEntryDic = {} # holds the values for the text fields that will be made
+    global entered # holds the output of those fields
+    entered = {}
+    for i in range(4):
+        num +=1
+        entered["Value%s" %num]= tk.StringVar() # uses % tags to make unique var names inside the dictionatry
+        entered["Value%s" %num].set("null") # sets it by default to null
+    num = 0 
+    for i in range(clicked.get()): # this loop goes for the number of fields you selected
+        num +=1
+        regEntryDic["regEntry%s" %num] = tk.Entry(frame) # uses % tags to make unique var names inside the dictionatry
+        regEntryDic["regEntry%s" %num].pack(pady=3) # packs that variable to the screen
+        regEntryDic["regEntry%s" %num].config(textvariable = entered["Value%s" %num])  #sets the texvariable to be the entered dictionary at this index
+    global RegSubmit
+    RegSubmit = tk.Button(frame, text="Submit", command=regSubmitEntry)
+    RegSubmit.pack(pady=3)
+
+def regSubmitEntry():
+    clearframe(clear=frame)
+    assignVariables()
+    closepage = tk.Label(frame, text="Now when you close this window it will listen for those words,\nif you want the program to stop close the terminal that pops up as well,\ndo not close the terminal until you want the program to stop.").pack()
+
+def streamMode(): # makes the menu and variables for the Twitch mode
+    clearframe(clear=frame)
+    comingsoon = tk.Label(frame, text="Coming Soon").pack()
+
+def assignVariables(): # assigns the variables
+    num = 0
+    global values
+    values = {} # list of variables for text entries
+    for i in range(4):
+        num+=1
+        values["val%s" %num] = entered["Value%s" %num].get() # sets each variable to its corisponding StringVar
+
+#End of Tkinter Functions
+
     
 
 
+#Start of tkinter loop
 global root
 root = tk.Tk()
 # getting window close to center
@@ -210,9 +231,6 @@ global clicked
 clicked = tk.IntVar()
 clicked.set("1")
 
-root.grid_rowconfigure(0, weight=0)
-root.grid_columnconfigure(0, weight=0)
-
 title = tk.Label(root, text="Hello and welcome to my game, My name is Code 9Mill and I live to make 'Inovative' programs like this.\nBellow their's some config options, if you get confused just press the tutorial button bellow.\nGood luck, and please give me credit for the hours of dedication I give to these seamingly useless programs.").grid(row=0,column=0)
 tutorial_button = tk.Button(root, text="Tutorial", width=10, command=takeTutorial).grid(row=1,column=0,pady=3)
 channel_button = tk.Button(root, text="Channel", width=10, command=takeChannel).grid(row=2,column=0,pady=3)
@@ -226,18 +244,19 @@ makeFrame()
 
 root.mainloop()
 
+#End of tkinter loop
+
+#Start of program after the menu
 playsound.playsound('Intoduction.mp3')
 
+triggerWord1 = values['val1'] # setting the variables to the trigger words
+triggerWord2 = values['val2'] # setting the variables to the trigger words
+triggerWord3 = values['val3'] # setting the variables to the trigger words
+triggerWord4 = values['val4'] # setting the variables to the trigger words
 
-triggerWord1 = values['val1']
-triggerWord2 = values['val2']
-triggerWord3 = values['val3']
-triggerWord4 = values['val4']
 
-
-get_Audio()
-
+get_Audio() # starts cycle 
 
 
 
-
+#Start of twitch bot
