907a0c0065e6a193a487b6cc0c0193dd72661eeb
jonahjack <youngthreatrapper@gmail.com>
2022-06-14 23:40:50 -0400

    Setting up Twitch Bot

---
+++ bot.py
@@ -0,0 +1,20 @@
+import os
+from twitchio.ext import commands
+
+# set up the bot
+bot = commands.Bot(
+    token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+@bot.event
+async def event_ready():
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+if __name__ == "__main__":
+    bot.run()
